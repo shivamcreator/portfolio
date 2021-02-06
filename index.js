3f8d7bc30568fefdf4acdea53d6e7b09d9789a2f
Raghav <rgandotra14@e.com>
2021-02-06 16:06:53 +0530

    some changes reagarding delay in animation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+firstLine = document.querySelectorAll('.left')[0];
+secondLine = document.querySelectorAll('.right')[0];
+thirdLine = document.querySelectorAll('.left')[1];
+fourthLine = document.querySelectorAll('.right')[1];
 
+if (screen.width>800) {
+  setInterval(left, 1000);
+  function left(){
+    firstLine.classList.add('leftmove');
+    thirdLine.classList.add('leftmove');
+    secondLine.classList.add('rightmove');
+    fourthLine.classList.add('rightmove');
+  }
+}
