5bfe43760ff4932d2bba3be71ea661037579c270
Raghav <rgandotra14@e.com>
2021-02-07 23:36:42 +0530

    changed

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let about=document.querySelector('.about-text');
-setInterval(mf,100);
+let dash= document.querySelector('.small-dash');
+let nav = document.querySelector('.upper-nav');
+let fix= nav.offsetTop;
+window.onscroll = function() {mf()};
 
 function mf() {
-  if (document.documentElement.scrollTop>=110) {
+  if (document.documentElement.scrollTop>=180) {
     about.classList.add("for-all");
+
+  }if (document.documentElement.scrollTop>=220) {
+    dash.classList.add("for-all");
+  }
+  if(window.pageYOffset > fix){
+    nav.classList.add("fix");
+  }else{
+    nav.classList.remove("fix");
   }
 }
