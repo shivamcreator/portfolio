98d231b9ade746decd2477f85061a2725e7ab1ca
Raghav <rgandotra14@e.com>
2021-02-11 23:58:10 +0530

    progress bar animation  added

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ let h1 = document.querySelector('.h1');
 let h2 = document.querySelector('.h2');
 let h3 = document.querySelector('.h3');
 let h4 = document.querySelector('.h4');
+let p1 = document.querySelector('.progress1');
+let p2 = document.querySelector('.progress2');
+let p3 = document.querySelector('.progress3');
+let p4 = document.querySelector('.progress4');
+let p5 = document.querySelector('.progress5');
+let p6 = document.querySelector('.progress6');
+let p7 = document.querySelector('.progress7');
+let p8 = document.querySelector('.progress8');
 let aboutheading1 = document.querySelectorAll('.about-heading')[0];
 let aboutheading2 = document.querySelectorAll('.about-heading')[1];
 let aboutheading3 = document.querySelectorAll('.about-heading')[2];
@@ -14,6 +22,7 @@ let ap1= document.querySelector('.ap1');
 let ap2= document.querySelector('.ap2');
 let ap3= document.querySelector('.ap3');
 let ap4= document.querySelector('.ap4');
+let progressDiv = document.querySelector('.progress-div');
 window.onscroll = function() {mf()};
 
 function mf() {
@@ -37,10 +46,44 @@ function mf() {
     ap2.classList.add("for-all2");
     ap3.classList.add("for-all2");
     ap4.classList.add("for-all2");
+  }if(document.documentElement.scrollTop>=750){
+    progressDiv.classList.add("for-all3");
+    setTimeout(load,750);
+    setTimeout(load2,850);
+    setTimeout(load3,950);
+    setTimeout(load4,1050);
+    setTimeout(load5,1150);
+    setTimeout(load6,1250);
+    setTimeout(load7,1350);
+    setTimeout(load8,1450);
   }
   if(window.pageYOffset > fix){
     nav.classList.add("fix");
   }else{
     nav.classList.remove("fix");
   }
+}
+function load8() {
+ p8.style.width = (450*55)/100 + 'px';
+}
+function load() {
+ p1.style.width = (450*90)/100 + 'px';
+}
+function load2() {
+ p2.style.width = (450*80)/100 + 'px';
+}
+function load3() {
+ p3.style.width = (450*70)/100 + 'px';
+}
+function load4() {
+ p4.style.width = (450*70)/100 + 'px';
+}
+function load5() {
+ p5.style.width = (450*50)/100 + 'px';
+}
+function load6() {
+ p6.style.width = (450*65)/100 + 'px';
+}
+function load7() {
+ p7.style.width = (450*40)/100 + 'px';
 }
